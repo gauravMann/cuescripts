666c1225c39175407c4cb258249b21f6d894a908
Gaurav Mann <gaurav.mann@cuemath.com>
2019-03-05 15:11:12 +0530

    [generatelist]Fixed install bug

--- generatelist/install.sh
+++ generatelist/install.sh
@@ -5,5 +5,5 @@ mkdir -p ~/workspace
 cd ~/workspace
 curl https://raw.githubusercontent.com/gauravMann/scripts/master/generatelist/file_to_list.sh > file_to_list.sh
 
-echo "alias fl='function _fl(){bash ~/workspace/file_to_list.sh $1};_fl'" >> ~/.zshrc
+echo "alias fl='function _fl(){bash ~/workspace/file_to_list.sh \$1};_fl'" >> ~/.zshrc
 source ~/.zshrc
