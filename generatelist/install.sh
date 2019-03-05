6a65a45cc381bcd5349b2c8b98c3f1e8999978bc
Gaurav Mann <gaurav.mann@cuemath.com>
2019-03-05 14:56:44 +0530

    [generatelist]Instll script added

---
+++ generatelist/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p ~/workspace
+cd ~/workspace
+curl https://raw.githubusercontent.com/gauravMann/scripts/master/generatelist/file_to_list.sh > file_to_list.sh
+
+echo "alias fl='function _fl(){bash ~/workspace/file_to_list.sh $1};_fl'" >> ~/.zshrc
+source ~/.zshrc
