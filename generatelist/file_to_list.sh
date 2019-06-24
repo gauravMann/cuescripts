d3af9c7b530c5c952c34063ca6d65f64d41975c9
Gaurav Mann <gauravMann@users.noreply.github.com>
2019-06-24 14:55:49 +0530

    piped pbpaste
    No need to create and delete file

--- generatelist/file_to_list.sh
+++ generatelist/file_to_list.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-pbpaste > fl.txt
-awk '{print "\x27",$0,"\x27"}' < fl.txt | tr '\n' ',' | tr -d "[:space:]"
-rm fl.txt
+pbpaste | awk '{print "\x27",$0,"\x27"}' | tr '\n' ',' | tr -d "[:space:]"
+
 
